70b1606791d67634f4f2fdff724a05f1db3d4c14
adefajem <127201816+adefajem@users.noreply.github.com>
2024-07-19 17:01:42 +0200

    Add files via upload

---
+++ Helsinki VRP-ADV version 2/functions.py
@@ -0,0 +1,239 @@
+import pandas as pd
+from pyproj import CRS, Transformer
+from geopy.distance import geodesic
+from docplex.mp.model import Model
+
+
+def convert_coordinates(x_3879, y_3879):
+    # Define the CRS for EPSG:3879 and WGS84
+    crs_3879 = CRS.from_epsg(3879)
+    crs_4326 = CRS.from_epsg(4326)
+    # Create a transformer object
+    transformer = Transformer.from_crs(crs_3879, crs_4326, always_xy=True)
+    # Perform the transformation
+    lon, lat = transformer.transform(x_3879, y_3879)    
+    return lat, lon
+
+
+def process_parcel_demand_data(parcel_demand_filename):    
+    parcel_demand_df = pd.read_csv(parcel_demand_filename)
+    # Get postcodes and convert X and Y coordinates to latitudes and longitudes
+    # Apply the conversion to each row
+    parcel_demand_df[['O_latitude', 'O_longitude']] = parcel_demand_df.apply(
+        lambda row: convert_coordinates(row['O_zone_X'], row['O_zone_Y']), axis=1, result_type='expand'
+    )
+
+    parcel_demand_df[['D_latitude', 'D_longitude']] = parcel_demand_df.apply(
+        lambda row: convert_coordinates(row['D_zone_X'], row['D_zone_Y']), axis=1, result_type='expand'
+    )
+
+    locations_df = parcel_demand_df[['O_latitude', 'O_longitude', 'D_latitude', 'D_longitude','Postcode_Ozone', 'Postcode_Dzone']]
+
+    # Group by D_latitude and D_longitude and count number of deliveries to that destination
+    grouped_df = locations_df.groupby(['D_latitude', 'D_longitude']).size().reset_index(name='Number_of_parcels_to_deliver')
+
+    # Merge the 'Number_deliveries' column back into the original dataframe
+    locations_df_with_counts = pd.merge(locations_df, grouped_df, on=['D_latitude', 'D_longitude'])
+
+    # Select unique rows based on D_zone_X and D_zone_Y
+    destinations_counts_df = locations_df_with_counts.drop_duplicates(subset=['D_latitude', 'D_longitude'])
+    destinations_counts_df.reset_index(inplace=True, drop=True)
+
+    unique_depot_df = locations_df.drop_duplicates(subset=['O_latitude', 'O_longitude'])
+    unique_depot_df = unique_depot_df[['O_latitude', 'O_longitude']]
+    unique_depot_df.rename(columns={'O_latitude': 'latitude', 'O_longitude': 'longitude'}, inplace=True)
+    unique_depot_df['postcode'] = destinations_counts_df['Postcode_Ozone'][0]
+    unique_depot_df['type'] = 'depot'
+
+
+    unique_destinations_df = pd.DataFrame()
+    unique_destinations_df['latitude'] = destinations_counts_df['D_latitude']
+    unique_destinations_df['longitude'] = destinations_counts_df['D_longitude']
+    unique_destinations_df['postcode'] = destinations_counts_df['Postcode_Dzone']
+    unique_destinations_df['type'] = 'destination'
+
+    all_nodes_df = pd.concat([unique_depot_df, unique_destinations_df], ignore_index=True)
+    return all_nodes_df, destinations_counts_df
+
+def generate_distance_time_matrices(nodes_df, robot_speed):
+    all_distances = []
+    all_times = []
+    avg_speed = robot_speed 
+    for i in range(len(nodes_df)):
+        origin = (nodes_df.loc[i]["latitude"], nodes_df.loc[i]["longitude"])
+        for j in range(len(nodes_df)):
+            destination = (nodes_df.loc[j]["latitude"], nodes_df.loc[j]["longitude"])            
+            dist = round(geodesic(origin, destination).meters , 2)
+            time = round(dist/avg_speed, 2)                
+            all_distances.append(dist)
+            all_times.append(time)
+            
+    # Create distance and time matrices
+    num_nodes = len(nodes_df)
+    distance_matrix = [all_distances[i:i + num_nodes] for i in range(0, len(all_distances), num_nodes)]
+    travel_time_matrix = [all_times[i:i + num_nodes] for i in range(0, len(all_times), num_nodes)]                
+    return distance_matrix, travel_time_matrix
+
+
+def generate_bigM_matrix(earliest, latest, wait_time, time_matrix):
+    bigM_matrix = []    
+    for i in range(len(time_matrix)):
+        temp = []
+        for j in range(len(time_matrix[i])):
+            temp.append(latest[i] + wait_time[i] + time_matrix[i][j])
+        bigM_matrix.append(temp)       
+    return bigM_matrix
+
+
+def robot_routing(distance_matrix, time_matrix, bigM_matrix, earliest, latest, wait_times, num_nodes):
+    model = Model(name = 'LMAD_routing')
+    model.parameters.timelimit = 30
+    # ----- Sets -----
+    K = range(1) #range(num_robots)
+    V = range(num_nodes)  
+    A = [(i,j) for i in V for j in V if i!=j]
+        
+    # ----- Variables ----- 
+    # x_kij = 1 if arc (i,j) is traversed by vehicle k  
+    x = {(k,i,j):model.binary_var(name='x_%d_%d_%d' % (k,i,j)) for k in K for i in V for j in V if i!=j}
+    
+    # Arrival time of vehicle k at node i - s_ki
+    s = {(k,i):model.continuous_var(lb=0.0, name='s_%d_%d' % (k,i)) for k in K for i in V}
+    
+    # ----- Objective function ----- 
+    model.minimize(model.sum(distance_matrix[i][j] * x[k,i,j] for k in K for (i,j) in A))         
+
+    # ----- Constraints -----
+    # Leave depot
+    for k in K:
+        model.add_constraint(model.sum(x[k,0,j] for j in V if j!=0) == 1, ctname = "leave depot")    
+   
+    # Visit all delivery nodes
+    for i in V:
+         model.add_constraint(model.sum(x[k,i,j] for k in K for j in V if i!=j) == 1, ctname = "visit all nodes")    
+    
+    # Flow conservation
+    for k in K:
+        for j in V:    
+            model.add_constraint(model.sum(x[k,i,j] for i in V if i!=j) - model.sum(x[k,j,i] for i in V if i!=j) == 0,
+                                ctname = "flow conservation")        
+    # Arrival times
+    for (i,j) in A:
+        if j==0: continue
+        for k in K:
+            model.add_constraint(s[k,i] + wait_times[i] + time_matrix[i][j] <= s[k,j] + (1 - x[k,i,j]) * bigM_matrix[i][j],
+                                ctname = "arrival times")
+         
+    # Respect time windows
+    for k in K:
+        for i in V:
+            model.add_constraint(earliest[i] <= s[k,i], ctname = "early window")
+            model.add_constraint(s[k,i] <= latest[i], ctname = "late window")
+         
+    # Solve
+    solution = model.solve(log_output = False)   
+    print(model.solve_status.name)
+    obj_val = None
+    sol_df = None
+
+    if model.solve_status.name == 'OPTIMAL_SOLUTION' or model.solve_status.name == 'FEASIBLE_SOLUTION':
+        obj_val = solution.get_objective_value()
+        sol_df = solution.as_df()    
+    return sol_df
+
+
+def EVCO2(kwh_consumption, emission_factor, generation_percentage):
+    energy_mix = sum([emission_factor[i] * generation_percentage[i] for i in range(len(emission_factor))])    
+    # if you multiply gCO2eq/kWh with kWh you get gCO2eq
+    emissions_gCO2 = kwh_consumption * energy_mix    
+    return emissions_gCO2 # in gCO2eq
+
+
+def get_emissions_matrix_EV(battery_capacity, emission_factor, generation_percentage, distance_matrix, travel_time_matrix, num_nodes):
+    emission_ij = []    
+    for i in range(len(distance_matrix)):
+        for j in range(len(distance_matrix)):              
+            if travel_time_matrix[i][j]!=0:
+                kwh_consumption = battery_capacity * distance_matrix[i][j] / 100000 # kilowatt-hours/100 km
+                emission = EVCO2(kwh_consumption, emission_factor, generation_percentage)
+                emission_ij.append(emission)
+            else:
+                emission_ij.append(0)                    
+    # Put in matrix form
+    E_ij = [emission_ij[i:i+num_nodes] for i in range(0, len(emission_ij), num_nodes)]                    
+    return E_ij
+
+
+def calculate_routing_emissions(sol_df, emissions_matrix_EV):
+    if sol_df is not None:
+        x_rows = sol_df[sol_df['name'].str.startswith('x_')]
+        x_rows.reset_index(inplace=True, drop=True)
+        # Remove possible duplicates
+        if len(x_rows) > 0:
+            for i in range(len(x_rows)):
+                if x_rows.loc[i]['value'] < 0.1:
+                    x_rows.drop([i], inplace=True)
+        x_rows.reset_index(inplace=True)
+        total_emissions_d = 0
+        for i in range(x_rows.shape[0]):
+            row = x_rows['name'][i].split('_')
+            row.pop(0);    
+            row = [int(i) for i in row]
+            total_emissions_d += emissions_matrix_EV[row[1]][row[2]]
+    else:
+        total_emissions_d = None
+    return total_emissions_d  
+
+
+def output_result(route_sol, all_nodes_df, destinations_counts_df, wait_times, average_serve_time_per_parcel):    
+    result = None    
+    # Extract times
+    t_rows = route_sol[route_sol['name'].str.startswith('s')]    
+    t_rows = t_rows.sort_values("value")
+    t_rows = t_rows.reset_index(drop=True)
+
+    # Get times at which the nodes are visited in HH:MM
+    t_df = pd.DataFrame(columns=['j','Arrival_time'])
+    for i in range(t_rows.shape[0]):
+        row = t_rows['name'][i].split('_')
+        row.pop(0);    
+        row = [int(i) for i in row]
+        j = row[-1]
+        hours = int(t_rows['value'][i] //3600)            
+        minutes = int((t_rows['value'][i] % 3600) // 60)
+        res = [j,"%02d:%02d" % (hours, minutes)]
+        t_df.loc[len(t_df)] = res
+
+    # Match node numbers to names
+    name_to_num_match = dict(zip(all_nodes_df['postcode'], list(all_nodes_df.index)))
+    names =[]
+    for i in range(len(t_df)):
+        for j in range(len(name_to_num_match)):
+            if t_df.loc[i]["j"] == list(name_to_num_match.values())[j]:
+                names.append(list(name_to_num_match.keys())[j])
+    t_df["Delivery_point"] = names
+    
+    # Get arrival times at delivery points
+    result = t_df[['Delivery_point','Arrival_time']].copy()           
+    
+    # Compute number of deliveries made based on problem instance input paramaters
+    postcode_and_counts = destinations_counts_df[['Postcode_Dzone','Number_of_parcels_to_deliver']]
+    postcode_and_counts = postcode_and_counts.rename(columns={'Postcode_Dzone': 'Delivery_point'})
+    result = pd.merge(result, postcode_and_counts, on='Delivery_point')
+    num_delivered = []
+    for i in range(len(list(wait_times[1:]))):
+        num_delivered.append(int(list(wait_times[1:])[i]/average_serve_time_per_parcel))
+
+    result['Number_of_parcels_delivered'] = num_delivered    
+    for p in range(len(result['Number_of_parcels_delivered'])):
+        if result['Number_of_parcels_delivered'][p] > result['Number_of_parcels_to_deliver'][p]:
+            result['Number_of_parcels_delivered'][p] = result['Number_of_parcels_to_deliver'][p]
+    
+    pctg_delivered = round(result['Number_of_parcels_delivered']/result['Number_of_parcels_to_deliver'], 2)
+    for j in range(len(pctg_delivered)):
+        if pctg_delivered[j] > 1:
+            pctg_delivered[j] = 1
+            
+    pctg_delivered = pctg_delivered * 100
+    result['Percentage_of_parcels_delivered_(%)'] = pctg_delivered    
+    return result
---
+++ Helsinki VRP-ADV version 2/main.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 16:04:44 2024
+
+@author: ade.fajemisin
+"""
+import pandas as pd
+pd.options.mode.chained_assignment = None
+import functions
+
+def main():
+    
+    # --- Inputs ---
+    # Take in parcel demand data
+    parcel_demand_filename = 'input/ParcelDemand.csv'
+    all_nodes_df, destinations_counts_df = functions.process_parcel_demand_data(parcel_demand_filename)
+    num_nodes = len(all_nodes_df)
+    
+    # Read in problem data
+    problem_input_filename = 'input/test_input_1.xlsx'
+    problem_input_data = pd.read_excel(problem_input_filename, sheet_name='problem_input')
+
+    earliest = problem_input_data.set_index('postcode')['earliest'].to_dict()
+    earliest = {k: v * 3600 for k, v in earliest.items()} # convert hours to seconds
+    earliest_list = [earliest[postcode] for postcode in all_nodes_df['postcode']] # map list order to order in all_nodes_df
+
+    latest = problem_input_data.set_index('postcode')['latest'].to_dict()
+    latest = {k: v * 3600 for k, v in latest.items()} # convert hours to seconds
+    latest_list = [latest[postcode] for postcode in all_nodes_df['postcode']] # map list order to order in all_nodes_df
+
+    wait_times = problem_input_data.set_index('postcode')['wait time (seconds)'].to_dict() 
+    wait_times_list = [wait_times[postcode] for postcode in all_nodes_df['postcode']] # map list order to order in all_nodes_df
+
+    # The robot moves on the sidewalk at walking speed, which is about 1.42 metres per second
+    robot_df = pd.read_excel(problem_input_filename, sheet_name='robot')
+    robot_speed = robot_df['robot speed (metres per second)'][0] 
+
+    # Electricity generation info - READ FROM FILE
+    electricity_inputs = pd.read_excel(problem_input_filename, sheet_name='electricity_generation_breakdwn')
+    emission_factor = list(electricity_inputs['Emission Factor'])
+    generation_percentage = list(electricity_inputs['Generation Percentage'])
+
+    # Battery capacity of electric vehicle in kWh - READ FROM FILE
+    battery_capacity = robot_df['battery capacity (kWh)'].values[0]
+
+    # Service time per parcel
+    average_serve_time_per_parcel = robot_df['average service time per parcel (seconds)'].values[0]
+
+    
+    # --- Do routing ---
+    distance_matrix, travel_time_matrix = functions.generate_distance_time_matrices(all_nodes_df, robot_speed)
+    bigM_matrix = functions.generate_bigM_matrix(earliest_list, latest_list, wait_times_list, travel_time_matrix)
+
+    try:
+        routing_solution = functions.robot_routing(distance_matrix, travel_time_matrix, bigM_matrix, earliest_list, latest_list, wait_times_list, num_nodes)
+    except:
+            print('The problem is infeasible. Ajust inputs and try again.')
+    
+    
+    # --- Output --- 
+    # Get total emissions
+    emissions_matrix_EV =  functions.get_emissions_matrix_EV(battery_capacity, emission_factor, generation_percentage, distance_matrix, travel_time_matrix, num_nodes)
+    total_emissions = functions.calculate_routing_emissions(routing_solution, emissions_matrix_EV)
+    emissions_df = pd.DataFrame([total_emissions], columns=['Total CO2 emissions (gCO2eq)'])
+
+    # Get robot arrival times at destination nodes and number of parcels delivered
+    result = functions.output_result(routing_solution, all_nodes_df, destinations_counts_df, wait_times_list, average_serve_time_per_parcel)
+    
+    # Write to file
+    result_filename = 'output/results_1.xlsx'
+    with pd.ExcelWriter(result_filename) as writer:  
+        result.to_excel(writer, sheet_name='Delivery_results', index = False)        
+        emissions_df.to_excel(writer, sheet_name='Total_emissions', index = False)
+
+
+    
+if __name__ == "__main__":
+    main()
